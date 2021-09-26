017efd30cb0f0540372eb8cada894d6f005467fb
Mpak1996 <giannismpak@outlook.com>
2021-09-27 01:40:50 +0300

    Upload preview photo of portfolio website

--- bundle.js
+++ bundle.js
@@ -4,8 +4,8 @@ const emailjs = require('emailjs-com')
 const submitForm = (e) => {
     e.preventDefault();
     emailjs.sendForm('Ioannis_Bakomichalis', 'Portfolio', ".contact-form", 'user_yWpIePOKymcYkdFEcMrL0')
-     .then(result => alert('Your message has been sent. I will be in contact with you soon.Thank you!'))
-     .catch(err => alert('Oops! Please try again.'))
+    .then(result => alert('Your message has been sent. I will be in contact with you soon.Thank you!'))
+    .catch(err => alert('Oops! Please try again.'))
 
 }
 
